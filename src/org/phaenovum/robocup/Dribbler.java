d68a24afcb5fa107f286a5a0a614815e9f896db7
Lars Möllendorf <lars@moellendorf.eu>
2019-02-22 02:43:42 +0100

    Add dribbler test

--- src/org/phaenovum/robocup/Dribbler.java
+++ src/org/phaenovum/robocup/Dribbler.java
@@ -2,8 +2,11 @@ package org.phaenovum.robocup;
 
 import lejos.hardware.Button;
 import lejos.hardware.Sound;
+import lejos.hardware.ev3.LocalEV3;
+import lejos.hardware.lcd.LCD;
 import lejos.hardware.motor.EV3LargeRegulatedMotor;
 import lejos.hardware.port.MotorPort;
+import lejos.robotics.navigation.OmniPilot;
 
 public class Dribbler {
 
@@ -22,16 +25,63 @@ public class Dribbler {
 		motor.rotate(-1440, true);
 	}
 
+	@SuppressWarnings("deprecation")
 	public static void main(String[] args) {
+		EV3LargeRegulatedMotor motorRight = new EV3LargeRegulatedMotor(MotorPort.B);
+		EV3LargeRegulatedMotor motorLeft = new EV3LargeRegulatedMotor(MotorPort.A);
+		EV3LargeRegulatedMotor motorBack = new EV3LargeRegulatedMotor(MotorPort.C);
+		OmniPilot pilot = new OmniPilot(89f, 64f, motorBack, motorLeft, motorRight, false, true,
+				LocalEV3.get().getPower());
 		EV3LargeRegulatedMotor motor = new EV3LargeRegulatedMotor(MotorPort.D);
 		Dribbler dribbler = new Dribbler(motor);
-		while (Button.ESCAPE.isUp()) {
-			dribbler.dribble();
-			Sound.pause(3000);
-			Sound.beep();
-			dribbler.release();
-			Sound.beepSequenceUp();
+
+		pilot.setLinearSpeed(100);
+		pilot.setAngularSpeed(10);
+		long time = System.currentTimeMillis();
+		int state = 0;
+		boolean go = true;
+
+		dribbler.dribble();
+
+		while (Button.ESCAPE.isUp() && go) {
+
+			if (time + 3000 < System.currentTimeMillis()) {
+				time = System.currentTimeMillis();
+				LCD.clear(3);
+				Sound.beep();
+				state++;
+			}
+
+			switch (state) {
+			case 0:
+				LCD.drawString("STAND", 0, 3);
+				break;
+			case 1:
+				LCD.drawString("FORWARD", 0, 3);
+				pilot.travel(100);
+				break;
+			case 2:
+				LCD.drawString("BACKWARD", 0, 3);
+				pilot.travel(-100);
+				break;
+			case 3:
+				LCD.drawString("LEFT", 0, 3);
+				pilot.rotate(360);
+				break;
+			case 4:
+				LCD.drawString("RIGHT", 0, 3);
+				pilot.rotate(-360);
+				break;
+			default:
+				dribbler.release();
+				Sound.beepSequenceUp();
+				go = false;
+				break;
+			}
+
 		}
+		pilot.stop();
+
 	}
 
 }
